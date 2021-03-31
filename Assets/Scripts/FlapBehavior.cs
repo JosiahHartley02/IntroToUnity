ad503b0029dfcfe338045a15688f2ac69c4397c2
Josiah Hartley <josiah.t.hartley@gmail.com>
2021-03-31 16:02:38 -0500

    Began Work On Making Pipes Lerp

--- Assets/Scripts/FlapBehavior.cs
+++ Assets/Scripts/FlapBehavior.cs
@@ -7,27 +7,49 @@ public class FlapBehavior : MonoBehaviour
     public float FlapForce = 1;
     public float MaxFallSpeed = 1;
     private Rigidbody _rigidbody;
+    public GameObject child;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+            Destroy(gameObject);
 
+        child.transform.parent = transform; 
+    }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Flap"))
+        if (Input.GetButtonDown("Flap"))
+        {
             _rigidbody.AddForce(new Vector3(0, FlapForce, 0), ForceMode.Impulse);
+            transform.Rotate(new Vector3(30,0,0), 30 * Time.deltaTime);
+        }
 
         if (_rigidbody.velocity.y < -MaxFallSpeed)
+        {
             _rigidbody.velocity = new Vector3(0, -MaxFallSpeed, 0);
+            transform.Rotate(new Vector3(0, 30, 0), 30 * Time.deltaTime);
+        }
         else if (_rigidbody.velocity.y > MaxFallSpeed)
+        {
             _rigidbody.velocity = new Vector3(0, MaxFallSpeed, 0);
+            transform.Rotate(new Vector3(0, 30, 0), 30 * Time.deltaTime);
+        }
 
         if (transform.position.x > 0)
+        {
             transform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
+            transform.Rotate(new Vector3(0, 0, 30), 30 * Time.deltaTime);
+        }
         else if (transform.position.x < 0)
+        {
             transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+            transform.Rotate(new Vector3(0, 0, 30), 30 * Time.deltaTime);
+        }
     }
 }
--- Assets/Scripts/MovementBehavior.cs
+++ Assets/Scripts/MovementBehavior.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class MovementBehavior : MonoBehaviour
 {
-    public float Speed = 1.0f;
+    public float Speed = 2.0f;
+    public float YMin = -5;
+    public float YMax = 4;
     // Start is called before the first frame update
     void Start()
     {
-
+        float yPos = Random.Range(YMin,YMax);
+        transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(-Speed, 0, 0) * Time.deltaTime;
+        float random = Random.Range(YMin, YMax);
+        transform.position -= new Vector3(Speed, 0, 0) * Time.deltaTime;
+        transform.position += Vector3.Lerp((new Vector3(transform.position.x, random, transform.position.z) * Time.deltaTime), (new Vector3(transform.position.x, random, transform.position.z) * Time.deltaTime),(Mathf.Cos(Time.time) / 2 + 0.5f) * Time.deltaTime);
     }
 }
